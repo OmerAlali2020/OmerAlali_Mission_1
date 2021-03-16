ac2c6cc29cf1904178d3858728d9bd3a10dcff0a
Omer Alali <omeralalisimantov@gmail.com>
2021-03-16 15:17:18 +0200

    Add algorithm

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ print('Pre - Fix code:', (abs(3.0*(4.0/3.0-1)-1)))
 print('Fix code:', (abs(3.0*(4.0/3.0-1)-1)-sys.float_info.epsilon))
 
 # Question 3:
-# find the machine precision
-print('The machine precision is:', sys.float_info.epsilon)
+# Find the machine precision by function:
+print('The machine precision is (func):', sys.float_info.epsilon)
+
+# Find the machine precision by algorithm:
+precision = 1
+
+while 1.0 + (precision/2) > 1.0:
+    precision = precision / 2
+print('The machine precision is (alg):', precision)
+
+# Both results is the same
